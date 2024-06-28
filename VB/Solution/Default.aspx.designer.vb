82689a1d49da826f38cb095565ce88621837d554
DevExpressExampleBot <fakeEmail>
2024-06-28 09:32:30 +0400

    Source auto update [skip ci]

--- VB/Solution/Default.aspx.designer.vb
+++ VB/Solution/Default.aspx.designer.vb
@@ -6,62 +6,65 @@
 '     the code is regenerated. 
 ' </auto-generated>
 '------------------------------------------------------------------------------
+
 Namespace Solution
 
-    Public Partial Class [Default]
 
-        ''' <summary>
-        ''' form1 control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
 
-        ''' <summary>
-        ''' grid control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected grid As Global.DevExpress.Web.ASPxGridView
+		''' <summary>
+		''' form1 control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected form1 As System.Web.UI.HtmlControls.HtmlForm
 
-        ''' <summary>
-        ''' rblDatasources control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected rblDatasources As Global.DevExpress.Web.ASPxRadioButtonList
+		''' <summary>
+		''' grid control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected grid As Global.DevExpress.Web.ASPxGridView
 
-        ''' <summary>
-        ''' dsProducts control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected dsProducts As Global.System.Web.UI.WebControls.AccessDataSource
+		''' <summary>
+		''' rblDatasources control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected rblDatasources As Global.DevExpress.Web.ASPxRadioButtonList
 
-        ''' <summary>
-        ''' dsCategories control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected dsCategories As Global.System.Web.UI.WebControls.AccessDataSource
+		''' <summary>
+		''' dsProducts control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected dsProducts As System.Web.UI.WebControls.AccessDataSource
 
-        ''' <summary>
-        ''' dsShippers control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected dsShippers As Global.System.Web.UI.WebControls.AccessDataSource
-    End Class
+		''' <summary>
+		''' dsCategories control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected dsCategories As System.Web.UI.WebControls.AccessDataSource
+
+		''' <summary>
+		''' dsShippers control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected dsShippers As System.Web.UI.WebControls.AccessDataSource
+	End Class
 End Namespace
--- VB/Solution/Default.aspx.vb
+++ VB/Solution/Default.aspx.vb
@@ -1,46 +1,50 @@
+﻿Imports DevExpress.Utils
 Imports DevExpress.Web
 Imports System
+Imports System.Linq
 
 Namespace Solution
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
 
-    Public Partial Class [Default]
-        Inherits Web.UI.Page
+		Private Enum DataSourceType
+			Products
+			Categories
+			Shippers
+		End Enum
+		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+			If Not Me.IsPostBack Then
+				Session.Clear()
+			End If
 
-        Friend Enum DataSourceType
-            Products
-            Categories
-            Shippers
-        End Enum
+			grid.DataBind()
+		End Sub
+		Protected Sub grid_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+			TryCast(sender, ASPxGridView).DataSource = GetDataSource()
+		End Sub
+		Protected Sub grid_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+			Session("selectedDataSource") = Int32.Parse(e.Parameters)
 
-        Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
-            If Not IsPostBack Then Session.Clear()
-            grid.DataBind()
-        End Sub
+			grid.Columns.Clear()
+			grid.AutoGenerateColumns = True
+			grid.KeyFieldName = String.Empty
+			grid.DataBind()
+		End Sub
+		Private Function GetDataSource() As Object
+			Dim o As Object = Session("selectedDataSource")
+			Dim dsType As DataSourceType = DataSourceType.Products
+			If o IsNot Nothing Then
+				dsType = DirectCast(o, DataSourceType)
+			End If
 
-        Protected Sub grid_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
-            TryCast(sender, ASPxGridView).DataSource = GetDataSource()
-        End Sub
-
-        Protected Sub grid_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
-            Session("selectedDataSource") = Integer.Parse(e.Parameters)
-            grid.Columns.Clear()
-            grid.AutoGenerateColumns = True
-            grid.KeyFieldName = String.Empty
-            grid.DataBind()
-        End Sub
-
-        Private Function GetDataSource() As Object
-            Dim o As Object = Session("selectedDataSource")
-            Dim dsType As DataSourceType = DataSourceType.Products
-            If o IsNot Nothing Then dsType = CType(o, DataSourceType)
-            Select Case dsType
-                Case DataSourceType.Categories
-                    Return dsCategories
-                Case DataSourceType.Shippers
-                    Return dsShippers
-                Case Else
-                    Return dsProducts
-            End Select
-        End Function
-    End Class
+			Select Case dsType
+				Case DataSourceType.Categories
+					Return dsCategories
+				Case DataSourceType.Shippers
+					Return dsShippers
+				Case Else
+					Return dsProducts
+			End Select
+		End Function
+	End Class
 End Namespace
